5c5c2982c2b075ebc0f644aeb75d96641079e2fb
Steve Callen <src3363@rit.edu>
2018-10-29 17:16:27 -0400

    did domomaker D, and fixed logout page

--- server/router.js
+++ server/router.js
@@ -2,8 +2,9 @@ const controllers = require('./controllers');
 const mid = require('./middleware');
 
 const router = (app) => {
+  app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
+  app.get('/getDomos', mid.requiresLogin, controllers.Domo.getDomos);
   app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
-  app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
   app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
   app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
